66ae9f4fdfc0e7dc324f0f523a1ab8db7c307960
Crowdbotics <team@crowdbotics.com>
2023-11-08 20:45:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fernhill-44425",
+  url: "https://fernhill-44425.botics.co",
+  api: "https://fernhill-44425.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
